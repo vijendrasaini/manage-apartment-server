896ffeb84a95c57554f49843f482b1a708586f18
Vijendra <docs.vijendra@gmail.com>
2022-04-17 19:23:31 +0530

    mongodb link is added

--- src/configs/database.js
+++ src/configs/database.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
-const mongoDB = "mongodb://localhost:27017/apartmentManagement-2"
+// const mongoDB = "mongodb://localhost:27017/apartmentManagement-2"
+const mongoDB = `mongodb+srv://vijendra:vijendra@cluster0.mpexw.mongodb.net/apartmentmanagement?retryWrites=true&w=majority`
 
 module.exports = ()=>mongoose.connect(mongoDB)
