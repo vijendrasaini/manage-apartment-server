618df9b281f2967a0b73df08c75396248d69449e
Vijendra <docs.vijendra@gmail.com>
2022-04-18 06:38:42 +0530

    routers for sign in and signup are  added

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const mongoConnector = require('./configs/database')
 
 const flatController = require('./controller/flat.controller')
+const { register, login } = require('./controller/user.controller')
 
 const app = express()
 app.use(express.json())
@@ -11,6 +12,9 @@ app.use(cors())
 const port = process.env.PORT || 7000
 
 app.use('/', flatController)
+app.use('/signup',register)
+app.use('/signin', login)
+
 
 module.exports = ()=>{
     app.listen(port, async ()=>{
