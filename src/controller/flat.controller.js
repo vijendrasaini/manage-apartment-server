e0d9723c9097411dba2f2b0e16acda57424efdc6
Vijendra <docs.vijendra@gmail.com>
2022-04-18 13:56:32 +0530

    filtering is fixed

--- src/controller/flat.controller.js
+++ src/controller/flat.controller.js
@@ -37,30 +37,42 @@ router.get('/flats',auth ,async (req, res)=>{
     try {
             const page = req.query.page || 1
             const limit = req.query.limit || 8
-            let flats = await Flat.find().skip((page - 1) * limit).limit(limit).lean().exec()
-            // {type : "owner"}).sort({no : 1}
-            // let flats = await Flat.find().lean().exec()
-            const totalDocs = await Flat.find().countDocuments()//flats.length || 0//
-            const totolPages = (Math.ceil(totalDocs/limit))
+            let totolPages = 0
+            let flats 
+            if(req.query.q){
+                if(req.query.q == 'sort')
+                {
+
+                    flats = await Flat.find().skip((page - 1) * limit).limit(limit).lean().exec()
+                    const totalDocs = await Flat.find().countDocuments()
+                    totolPages = (Math.ceil(totalDocs/limit))
+                    flats = req.query.sort == 1 ? flats.sort((a,b)=>(a.no - b.no)) : flats.sort((a,b)=>(-a.no + b.no))
+                }
+                else if(req.query.q == 'filter')
+                {
+                    flats = await Flat.find({type : req.query.base}).skip((page - 1) * limit).limit(limit).lean().exec()
+                    const totalDocs = await Flat.find({type : req.query.base}).countDocuments()
+                    totolPages = (Math.ceil(totalDocs/limit))
+                }
+                else
+                {
+                    flats = await Flat.find({block : req.query.block}).skip((page - 1) * limit).limit(limit).lean().exec()
+                    const totalDocs = await Flat.find({block : req.query.block}).countDocuments()
+                    totolPages = (Math.ceil(totalDocs/limit))
+                }
+            }
+            else
+            {
+                flats = await Flat.find().skip((page - 1) * limit).limit(limit).lean().exec()
+                const totalDocs = await Flat.find().countDocuments()
+                totolPages = (Math.ceil(totalDocs/limit))
+            }
             let arr = []
             for(let i =1;i<=totolPages;i++)
                 arr.push(i)
-            if(req.query.q){
-                if(req.query.q == 'sort'){
-                    flats = req.query.sort == 1 ? flats.sort((a,b)=>(a.no - b.no)) : flats.sort((a,b)=>(-a.no + b.no))
-                }
-                else if(req.query.q == 'filter'){
-                    flats = flats.filter(flat => flat.type == req.query.base)
-                }
-                else if(req.query.q == "search"){
-                    flats = flats.filter(flat => flat.block == req.query.block)
-                }
-            }
-
             return res
             .status(200)
             .send({ flats, totolPages : arr})
-            // .send({ flats})
     } catch (error) {
         return res
         .status(500)
